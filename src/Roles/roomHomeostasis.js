d7deb827dac2b07b284cc6b6e212894024264cac
Krotera <krotera@tuta.io>
2017-10-18 14:46:44 -0400

    Add tower defense, RCL population targets

--- src/Roles/roomHomeostasis.js
+++ src/Roles/roomHomeostasis.js
@@ -164,11 +164,11 @@ const roomHomeostasis = {
  * @param roomName
  */
 function towersDefendRoom(roomName) {
-    var hostiles = Game.rooms[roomName].find(FIND_HOSTILE_CREEPS);
-    if(hostiles.length > 0) {
-        var username = hostiles[0].owner.username;
+    let hostiles = Game.rooms[roomName].find(FIND_HOSTILE_CREEPS);
+    if (hostiles.length > 0) {
+        let username = hostiles[0].owner.username;
         Game.notify(`User ${username} spotted in room ${roomName}`);
-        var towers = Game.rooms[roomName].find(
+        let towers = Game.rooms[roomName].find(
             FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
         towers.forEach(tower => tower.attack(hostiles[0]));
     }
